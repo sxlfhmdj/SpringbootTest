f2059a05c1d5f95982fdd35d71fa56f8bb5657b4
Deng-J <sxlfhmdj@163.com>
2018-04-24 11:45:08 +0800

    Update MyObjectProxy.java

--- SpringBoot/src/test/java/proxy/test/staticProxy/MyObjectProxy.java
+++ SpringBoot/src/test/java/proxy/test/staticProxy/MyObjectProxy.java
@@ -1,11 +1,5 @@
 package proxy.test.staticProxy;
 
-/**
- * <p>Description: []</p>
- * Created on: 2018/4/23
- *
- * @author dengjiang_v@didichuxing.com
- */
 public class MyObjectProxy implements MyObject {
 
     private MyObjectImpl myObject;
